ec4165b4b71a51b8818cd843245d2179f758cd1e
Daniel Klauck <daniel_klauck@hotmail.com>
2022-01-27 11:38:56 -0300

    ALterações de Commit

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
-url = "bytebank.com/cambio?moedaOrigem=real"
+#url = "https://bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
+
+url = ""
+
+#Sanitização da URL
+url = url.strip()
+
+#Validação da URL
+if url == "":
+    raise ValueError("A URL está vazia")
 
 print(url)
 
-url_base = url[0:19]
-print(url_base)
+#Separa base e parâmetros
+indice_interrogacao = url.find("?")
+url_base = url[ : indice_interrogacao]
+url_parametros = url[indice_interrogacao + 1 : ] #sem parametros apos os : dai vai até o final da string
 
-url_parametros = url[20:36]
+#Busca o valor de um parâmetro
+parametro_busca = "moedaOrigem"
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+
+indice_e_comercial = url_parametros.find("&", indice_valor)
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor : ]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+
+print(valor)
+
+
 print(url_parametros)
